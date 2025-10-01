8bec2bb78e338cb7c7da9eb208e06b55ae07e052
Viktar Makayed <vmakoed@gmail.com>
2025-10-01 15:41:07 +0200

    Fix link_to vulnerability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def sanitize_url(url)
+    return nil if url.blank?
+
+    uri = URI.parse(url)
+    # Only allow http and https protocols
+    if uri.scheme.in?(%w[http https])
+      url
+    else
+      nil
+    end
+  rescue URI::InvalidURIError
+    nil
+  end
 end
